3180f185cef4add6b135a086a85c064abc697ff6
utin-francis-peter <u.francispeter@gmail.com>
2024-01-04 09:04:06 +0100

    style: adjusted slides styles

--- components/WhyCredussSwiper.jsx
+++ components/WhyCredussSwiper.jsx
@@ -10,14 +10,11 @@ import "swiper/css/pagination";
 // import required modules
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
-import SwiperButtonNext from "./SwiperButtonNext";
-import SwiperButtonPrev from "./SwiperButtonPrev";
-
 const WhyCredussSwiper = () => {
   return (
-    <div className="why-creduss-swiper text-black">
+    <div className="why-creduss-swiper text-black ">
       <Swiper
-        className="mySwiper md:w-[90%]"
+        className="mySwiper md:w-[90%] "
         modules={[Pagination, Navigation, Autoplay]}
         spaceBetween={50}
         slidesPerView={1}
@@ -28,17 +25,15 @@ const WhyCredussSwiper = () => {
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
-        }}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}>
+        }}>
         {WhyCredussList.map((item) => (
-          <SwiperSlide key={item.id}>
-            <div className="md:w-1/2">
-              <h1 className="font-bold mb-3">{item.title}</h1>
+          <SwiperSlide key={item.id} className=" ">
+            <div className="md:w-1/2 ">
+              <h1 className="text-2xl font-bold mb-3">{item.title}</h1>
               <p className="text-justify text-desc">{item.text}</p>
             </div>
 
-            <div className="md:w-1/2">
+            <div className="md:w-1/2 swiper-img">
               <Image
                 width={400}
                 height={400}
